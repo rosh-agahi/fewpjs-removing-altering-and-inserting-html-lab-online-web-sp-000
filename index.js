3fe10a53b3b86bb8d998a4cf8b0b710723cd999c
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-11 07:12:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector("#main")
+element.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = 'Roshy is the champion'
+document.body.appendChild(newHeader)
